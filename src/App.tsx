9d159e83da9a04ff185b2690396093d7a82d63df
Lucero <lucerogoga63@hotmail.com>
2023-06-07 16:57:43 -0500

    subir fuentes

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,18 @@ function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="App">
-      <button className="boton1">Agregar Empresa</button>
-      <button className="boton2">Agregar Empresa</button>
-      <button className="boton3">Agregar Empresa</button>
+    <div>
+      <h1 className="title">PROMART ES COOL</h1>
+      <div className="App">
+        <button className="boton1">Agregar Empresa</button>
+        <button className="boton2">Agregar Empresa</button>
+        <button className="boton3">Agregar Empresa</button>
+      </div>
+      <h1 className="prueba1">Agregar Empresa</h1>
+      <h2 className="prueba2">Agregar Empresa</h2>
+      <h3 className="prueba3">Agregar Empresa</h3>
+      <h4 className="prueba4">Agregar Empresa</h4>
+      <p className="prueba5">Agregar Empresa</p>
     </div>
   );
 }
